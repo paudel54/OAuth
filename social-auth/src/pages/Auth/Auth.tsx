b657df5c7d02e9d3e5b84fb74877a900259ea092
paudel54 <2sansrit@gmail.com>
2023-08-09 19:08:28 +0545

    implemented form validation with react hook form

--- social-auth/src/pages/Auth/Auth.tsx
+++ social-auth/src/pages/Auth/Auth.tsx
@@ -1,12 +1,32 @@
-import { link } from "fs";
 import { authClasses } from "./authClasses";
+import { useForm } from "react-hook-form";
+// adds validation rules defined in Yup schema to form fields
+import { yupResolver } from "@hookform/resolvers/yup";
+import { AuthForm } from "../../models/Form";
+import { authFormSchema } from "../../models/Form";
 const { button, hr, forgotPasswordButton } = authClasses;
 
+const handleFormSubmit = (data: AuthForm) => {
+  console.log("Check out the TypeScript Type Checked Data.", data);
+};
+
 const Auth = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<AuthForm>({
+    resolver: yupResolver(authFormSchema),
+  });
+
   return (
     <div className="grid h-screen place-items-center px-4 text-sm font-medium">
       <div className="bg-red-300 w-full max-w-sm rounded-lg bg-slate-700/30 shadow">
-        <form className="p-4 md:p-5 lg:p-6">
+        {/* Passing on custom form to submit handler */}
+        <form
+          onSubmit={handleSubmit(handleFormSubmit)}
+          className="p-4 md:p-5 lg:p-6"
+        >
           <div className="grid gap-y-3">
             <button type="button" className={button}>
               Google
@@ -22,24 +42,45 @@ const Auth = () => {
           <div className="grid gap-y-3">
             <div>
               <input
+                {...register("email")}
                 type="email"
                 placeholder="email@gmail.com"
                 className="focus:border-purple-400 w-full rounded-md border border-slate-600 bg-gray-700  text-white p-2"
               />
+              {errors.email ? (
+                <span className="text-red-700">{errors.email.message}</span>
+              ) : (
+                <></>
+              )}
             </div>
             <div>
               <input
+                {...register("password")}
                 type="password"
                 placeholder="Input Password"
                 className="focus:border-purple-400 w-full rounded-md border border-slate-600 bg-gray-700 text-white p-2"
               />
+
+              {errors.password ? (
+                <span className="text-red-700">{errors.password.message}</span>
+              ) : (
+                <></>
+              )}
             </div>
             <div>
               <input
+                {...register("confirmPassword")}
                 type="password"
                 placeholder="Confirm Password"
                 className="focus:border-purple-400 w-full rounded-md border border-slate-600 bg-gray-700 text-white p-2"
               />
+              {errors.confirmPassword ? (
+                <span className="text-red-700">
+                  {errors.confirmPassword.message}
+                </span>
+              ) : (
+                <></>
+              )}
             </div>
             <button className={button}>Sign in with email</button>
           </div>
